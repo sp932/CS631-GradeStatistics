38a092e806760d920d1e6d86e22c18dd745fe77a
sp932 <sapanpatel@Sapans-MacBook-Pro.local>
2016-12-10 14:05:39 -0500

    adding grade-statistics wireframe

---
+++ grade-statistics/student-roster.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="images/favicon.ico">
+
+<title>Student Roster</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	  <link href="css/style.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="css/dataTables.tableTools.css" rel="stylesheet">
+    <link href="css/datepicker.css" rel="stylesheet">
+
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/dataTables.bootstrap.js"></script>
+    <script src="js/dataTables.tableTools.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+
+<script>
+ $(document).ready( function () {
+    $("#myTable").dataTable( {
+        "dom": '<"clear">f',
+        "paging": false,
+        "searching": true,
+    } );
+} );
+ </script>
+
+
+</head>
+
+<body>
+  <nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="student-roster.php">Student Roster<span class="sr-only">(current)</span></a></li>
+          <li><a href="student-grade.php">Student Grade</a></li>
+          <li><a href="exam-question-average.php">Exam Question Average</a></li>
+          <li><a href="student-exam-history.php">Student Exam History</a></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+
+      <?php
+	include 'login.php';
+	// connect to server and test if successful
+	$connection = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
+
+if(mysqli_connect_error()){
+        die("Database Connection Failed: " .
+                mysqli_connect_error() .
+                " (" . mysqli_connect_errno() . ")"
+);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+?>
+
+              <br/>
+
+    </tbody>
+
+    </table>
+
+
+  </div><!-- /container -->
+
+
+
+</body>
+</html>
